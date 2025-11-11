76de5897f0d77aaa140e4f2612eb0d5a734f13b1
WeDontKnow3 <marquinhorelampago004@gmail.com>
2025-11-11 10:38:15 -0300

    Create MonoEmoji.jsx

---
+++ src/components/MonoEmoji.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default function MonoEmoji({ children, label = '', className = '', style = {} }) { return ( <span role="img" aria-label={label} className={mono-emoji ${className}} style={style} > {children} ); }
